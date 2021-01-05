0c92c78425da9c6dfe8576b75ef89ac6eaef20fe
Michael Andrikanich <mandrikanich@gmail.com>
2021-01-05 20:19:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,8 @@
+mixed_data_1 = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
 def join_nested_strings(src)
 row_index = 0 
 string_array = [] 
@@ -16,8 +21,3 @@ end
  end 
 join_nested_strings(mixed_data_1)
 
-mixed_data_1 = [
-  ["The", 4, "quick"],
-  [-1, "brown", "fox", 30],
-  ["studied", 101, 233, "Ruby"]
-]
